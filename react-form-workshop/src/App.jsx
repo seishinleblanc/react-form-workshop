55ac6dd520c71ba7abcfecd266b76d4679bed43b
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-07-28 13:00:44 -0500

    Created all components for workshop.

--- react-form-workshop/src/App.jsx
+++ react-form-workshop/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 
+import SignUpForm  from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState(null)
 
   return (
     <>
-      
-     
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token} />
     </>
   )
 }
---
+++ react-form-workshop/src/components/SignUpForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+
+export default function SignUpForm( {setToken} ) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/signup`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            }
+            )
+            const data = await response.json();
+
+            setToken(data.token)
+
+            setUsername('')
+            setPassword('')
+
+        } catch (error) { 
+            setError(error.message);
+        }
+    }
+    
+    return <>
+        <h2> Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+            <label> Username: 
+                <input value={username} onChange={(event) => setUsername(event.target.value)} />
+            </label>
+            <label> Password: 
+                <input value={password} onChange={(event) => setPassword(event.target.value)} />
+            </label>
+            <button>Submit</button>
+        </form>
+    </>
+}
